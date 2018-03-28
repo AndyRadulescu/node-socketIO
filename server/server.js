bca13515d267a86ceff011a5e88eccd1cff61f52
Andy Radulescu <radulescu.eduard.andrei@gmail.com>
2018-03-28 17:08:36 +0300

    set the engines for deployment

--- server/server.js
+++ server/server.js
@@ -2,14 +2,15 @@ const path = require('path');
 const express = require('express');
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
+
 const publicPath = path.join(__dirname, '../public');
 console.log(publicPath);
 
 app.use(express.static(publicPath))
 
-app.listen(3000, () => {
-    console.log(`Started on port ${3000}`);
+app.listen(port, () => {
+    console.log(`Started on port ${port}`);
 });
 
 module.exports = {app};
